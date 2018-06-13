ec373298682cbf6f6c771305476b4290eab8bd55
farid manafov <faridbardium@gmail.com>
2018-06-13 18:00:16 +0000

    pushed updated to exercise deleting url

---
+++ express_server.js
@@ -0,0 +1,74 @@
+var express = require("express");
+var app = express();
+var PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
+var randomstring = require("randomstring");
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.set("view engine", "ejs");  
+
+var urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+// Hello!
+app.get("/", (req, res) => {
+  res.end("Hello!");
+});
+
+//
+app.get("/urls", (req, res) => {
+    let databaseObject = {urls : urlDatabase}
+    res.render('urls_index', databaseObject)
+})
+
+app.post("/urls_index", (req, res) =>{
+  
+})
+
+// RNG saver
+app.post("/urls", (req, res) => {
+  // console.log(urlDatabase[random]);  // debug statement to see POST parameters
+  randomShortUrl = randomstring.generate(6);
+  urlDatabase[randomShortUrl] = req.body.longURL;
+  console.log(urlDatabase);
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  
+});
+//redirection
+app.get("/u/:shortURL", (req, res) => {
+  let longURL = urlDatabase[req.params.shortURL]
+  res.redirect(longURL);
+});
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+app.get("/hello", (req, res) => {
+    res.end("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
+
+// body parser
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+
+//~~~~~~~~
+
+// :id grabber of shortURL that displays short and long urls
+
+app.get("/urls/:id", (req, res) => {
+  let templateVars = {shortURL: req.params.id, longURL: urlDatabase[req.params.id]};
+  res.render("urls_show", templateVars)
+
+
+});
